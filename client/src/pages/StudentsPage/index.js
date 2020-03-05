80e1c28b7db47427c5f76fa79501665d572e45ee
kirsenkodanit <a.kirs@ukr.net>
2020-03-05 12:37:33 +0200

    created table component & table test component with fake data&action

--- client/src/pages/StudentsPage/index.js
+++ client/src/pages/StudentsPage/index.js
@@ -4,22 +4,76 @@ import { DataTable } from '../../components';
 export const StudentsPage = props => {
   const params = {
     columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+      { title: 'Full name', field: 'fullName' },
+      { title: 'Passport', field: 'passport' },
+      { title: 'Date of birth', field: 'dateOfBirth', type: 'string' },
       {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        title: 'Group',
+        field: 'group',
+        lookup: { 1: '1', 2: '2', 3: '3' },
+        
       },
     ],
     data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
       {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
+        fullName: 'Ivanov Sergii',
+        passport: 'СК234567',
+        dateOfBirth: '30.04.1960',
+        group: 1,
+      },
+      {
+        fullName: 'Ivanov Oleksandr',
+        passport: 'СК345678',
+        dateOfBirth: '20.02.1980',
+        group: 3,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
+      },
+      {
+        fullName: 'Ivanov Ivan',
+        passport: 'СК123456',
+        dateOfBirth: '10.02.1970',
+        group: 2,
       },
     ],
     name: 'Students',
